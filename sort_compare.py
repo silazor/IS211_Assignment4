de95db876de52b3f0f392aa880341afda9417ff5
silazor <42361997+silazor@users.noreply.github.com>
2019-09-23 14:02:27 -0400

    Add files via upload

--- sort_compare.py
+++ sort_compare.py
@@ -3,172 +3,83 @@ from random import seed
 from random import randint
 import pprint
 
-def sequential_search(big_list, item):
+def insertion_sort(a_list):
     start = time.time()
-    for a_list in big_list:
-        # These are initializers
-        pos = 0
-        found = False
-        # do the stuff belove
-        # This loops through a single array
-        while pos < len(a_list) and not found:
-            if a_list[pos] == item:
-                found = True
-                end = time.time()
-                elapsed = end-start
-                return (found, elapsed)
-            else:
-                pos = pos+1
+    for index in range(1, len(a_list)):
+        current_value = a_list[index]
+        position = index
+        while position > 0 and a_list[position - 1] > current_value:
+            a_list[position] = a_list[position - 1]
+            position = position - 1
+        a_list[position] = current_value
     end = time.time()
     elapsed = end-start
+    return elapsed
 
-    return(found, elapsed)
 
-def ordered_sequential_search(big_list, item):
+def shell_sort(a_list):
     start = time.time()
-    for ordered_sequential_list in big_list:
-        pos = 0
-        found = False
-        stop = False
-        while pos < len(ordered_sequential_list) and not found and not stop:
-            if ordered_sequential_list[pos] == item:
-                found = True
-                end = time.time()
-                elapsed = end-start
-                return (found, elapsed)
-            else:
-                if ordered_sequential_list[pos] > item:
-                    stop = True
-                else:
-                    pos = pos+1
+    sublist_count = len(a_list) // 2
+    while sublist_count > 0:
+        for start_position in range(sublist_count):
+            gap_insertion_sort(a_list, start_position, sublist_count)
+        sublist_count = sublist_count // 2
     end = time.time()
     elapsed = end-start
-    return(found, elapsed)
+    return elapsed
 
-def binary_search():
+def gap_insertion_sort(a_list, start, gap):
+    for i in range(start + gap, len(a_list), gap):
+        current_value = a_list[i]
+        position = i
+        while position >= gap and a_list[position - gap] > current_value:
+            a_list[position] = a_list[position - gap]
+            position = position - gap
+            a_list[position] = current_value
+
+
+def python_sort(a_list):
     start = time.time()
-    #code
+
+    a_list.sort()
+
     end = time.time()
     elapsed = end-start
-    pass
-
-def binary_search_recursive(binary_recursive_list, item):
-    if len(binary_recursive_list) == 0:
-        return False
-    else:
-        midpoint = len(binary_recursive_list)//2
-        if binary_recursive_list[midpoint] == item:
-            return True
-        else:
-            if item < binary_recursive_list[midpoint]:
-                return binary_search_recursive(binary_recursive_list[:midpoint], item)
-            else:
-                return binary_search_recursive(binary_recursive_list[midpoint + 1:], item)
+    return elapsed
 
 def generate_arrays(length):
     seed(1)
-    big_ol_array = []
-    for num in range(0, 100):
-        #print(f"making array num---> {num}")
-        lil_array_holder = []
-        for _ in range(length):
-            value = randint(1, 100)
-            lil_array_holder.append(value)
-        #print(f"The size of lil_array_holder is --> {len(lil_array_holder)}")
-        big_ol_array.append(lil_array_holder)
-    return big_ol_array
-    #pprint.pprint(big_ol_array)
-    #print(f"The size of our big_ol_array is ---> {len(big_ol_array)}")
-
-def sorted_list(big_list):
-    new_big_list = []
-    for little_list in big_list:
-        little_list.sort()
-        new_big_list.append(little_list)
-    return new_big_list
-
-def binary_search_iterative(big_list, item):
-    start = time.time()
-    for binary_iterative_list in big_list:
-        first = 0
-        last = len(binary_iterative_list) - 1
-        found = False
-        while first <= last and not found:
-            midpoint = (first + last) // 2
-            if binary_iterative_list[midpoint] == item:
-                found = True
-                end = time.time()
-                elapsed = end-start
-                return(found, elapsed)
-            else:
-                if item < binary_iterative_list[midpoint]:
-                    last = midpoint - 1
-                else:
-                    first = midpoint + 1
-    end = time.time()
-    elapsed = end-start
-    return(found, elapsed)
+    big_array = []
+    for _ in range(length):
+        value = randint(1, 100)
+        big_array.append(value)
+    return big_array
 
 def main():
-    results = {}
-    array_100_with_500_ints = generate_arrays(500)
-    array_100_with_1000_ints = generate_arrays(1000)
-    array_100_with_10000_ints = generate_arrays(10000)
+    array_with_500_ints = generate_arrays(500)
+    array_with_1000_ints = generate_arrays(1000)
+    array_with_10000_ints = generate_arrays(10000)
 
-    sorted_500 = sorted_list(array_100_with_500_ints)
-    sorted_1000 = sorted_list(array_100_with_1000_ints)
-    sorted_10000 = sorted_list(array_100_with_10000_ints)
+    total_time = insertion_sort(array_with_500_ints)
+    print("Insertion sort 500 took %10.7f seconds to run, on average." %total_time)
+    total_time = insertion_sort(array_with_1000_ints)
+    print("Insertion sort 1000 took %10.7f seconds to run, on average." %total_time)
+    total_time = insertion_sort(array_with_10000_ints)
+    print("Insertion sort 10000 took %10.7f seconds to run, on average." %total_time)
 
-    found, total_time = sequential_search(array_100_with_500_ints, -1)
-    print("Sequential search 500 took %10.7f seconds to run, on average." %total_time)
-    found, total_time = sequential_search(array_100_with_1000_ints, -1)
-    print("Sequential search 1000 took %10.7f seconds to run, on average." %total_time)
-    found, total_time = sequential_search(array_100_with_10000_ints, -1)
-    print("Sequential search 10000 took %10.7f seconds to run, on average." %total_time)
+    total_time = shell_sort(array_with_500_ints)
+    print("Shell sort 500 took %10.7f seconds to run, on average." %total_time)
+    total_time = shell_sort(array_with_1000_ints)
+    print("Shell sort 1000 took %10.7f seconds to run, on average." %total_time)
+    total_time = shell_sort(array_with_10000_ints)
+    print("Shell sort 10000 took %10.7f seconds to run, on average." %total_time)
 
-    found, total_time = ordered_sequential_search(sorted_500, -1)
-    print("Ordered sequential search 500 took %10.7f seconds to run, on average." %total_time)
-    found, total_time = ordered_sequential_search(sorted_1000, -1)
-    print("Ordered sequential search 1000 took %10.7f seconds to run, on average." %total_time)
-    found, total_time = ordered_sequential_search(sorted_10000, -1)
-    print("Ordered sequential search 10000 took %10.7f seconds to run, on average." %total_time)
-
-    found, total_time = binary_search_iterative(sorted_500, -1)
-    print("Binary search iterative 500 took %10.7f seconds to run, on average." %total_time)
-    found, total_time = binary_search_iterative(sorted_1000, -1)
-    print("Binary search iterative 1000 took %10.7f seconds to run, on average." %total_time)
-    found, total_time = binary_search_iterative(sorted_10000, -1)
-    print("Binary search iterative 10000 took %10.7f seconds to run, on average." %total_time)
-
-    start = time.time()
-    for list in sorted_500:
-        binary_search_recursive(list, -1)
-    end = time.time()
-    elapsed = end-start
-    print("Binary search recursive 500 took %10.7f seconds to run, on average." %elapsed)
-
-    start = time.time()
-    for list in sorted_1000:
-        binary_search_recursive(list, -1)
-    end = time.time()
-    elapsed = end-start
-    print("Binary search recursive 1000 took %10.7f seconds to run, on average." %elapsed)
-
-    start = time.time()
-    for list in sorted_1000:
-        binary_search_recursive(list, -1)
-    end = time.time()
-    elapsed = end-start
-    print("Binary search recursive 10000 took %10.7f seconds to run, on average." %elapsed)
-    #found, time = binary_search_recursive(sorted_500, -1)
-    #print("Sequential search 500 took %10.7f seconds to run, on average." %time)
-    #found, time = binary_search_recursive(sorted_1000, -1)
-    #print("Sequential search 1000 took %10.7f seconds to run, on average." %time)
-    #found, time = binary_search_recursive(sorted_10000, -1)
-    #print("Sequential search 10000 took %10.7f seconds to run, on average." %time)
-
-
-    pass
+    total_time = python_sort(array_with_500_ints)
+    print("Python sort 500 took %10.7f seconds to run, on average." %total_time)
+    total_time = python_sort(array_with_1000_ints)
+    print("Python sort 1000 took %10.7f seconds to run, on average." %total_time)
+    total_time = python_sort(array_with_10000_ints)
+    print("Python sort 10000 took %10.7f seconds to run, on average." %total_time)
 
 if __name__ == '__main__':
     main()
